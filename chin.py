27b535d4a4e0671199c35d8d69982466f3bbda1e
Alex Clemmer <clemmer.alexander@gmail.com>
2010-06-15 18:47:45 -0600

    Most initial parsing legwork done

--- chin.py
+++ chin.py
@@ -27,6 +27,10 @@ def generateTagOpen(id):
 def generateTagClose():
 	return "</div>"
 
+def generateJs():
+	print "this is a stub for later"
+
+
 # Open file from argument
 raw = open(sys.argv[1], 'r')
 
@@ -36,9 +40,10 @@ lines = raw.readlines()
 # Create a list to hold the ByteSpan objects
 bss = list()
 
+
 # Process each line in succession
-for line in lines:
-	words = line.split()
+for line in range(1, len(lines)):
+	words = lines[line].split()
 	byteRange = words[1].split(",")
 	id = filter(lambda x: x in '1234567890', words[5])
 	bs = ByteSpan(int(byteRange[0]), int(byteRange[1]), int(id))
@@ -48,28 +53,24 @@ for line in lines:
 overlay = open("corefoverlay.html", 'w')
 mirror = open(sys.argv[2], 'rb')
 
+print "<html><head></head>\n<body>\n"
+
 index = 0
 # Begin generating html file
 for item in bss:
-	item.printargs() #debug
+	#item.printargs() #debug
+	if item.getStart() < index:
+		mirror.seek(item.getStart())
+		index = item.getStart()
 	# while placeholder <= start of bytespan
 	while index < item.getStart():
 		print mirror.read(1);
 		index+=1
+	print generateTagOpen(item.getCorefId())
 	# while placeholder in bytespan
 	while index <= item.getEnd():
 		print mirror.read(1)
 		index+=1
+	print generateTagClose()
 
-
-#for item in bss:
-#	while index < item.getStart():
-#		overlay.write(mirror.read(index))
-#		index = index + 1
-#	index = item.getStart()
-#	overlay.write(generateTagOpen(item.getCorefId()))
-#	index = index+1
-#	for n in range(index, item.getEnd()):
-#		overlay.write(mirror.read(1))
-#		index = index+1
-#	overlay.write(generateTagClose())
+print "\n</body>\n</html>"
