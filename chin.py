5f1340cc93e58899296e0a169ecabe2c91c38908
Alex Clemmer <clemmer.alexander@gmail.com>
2010-06-17 19:57:47 -0600

    Massive changes to how the bytespan data is handled
    Changes include storing the bytespan objects in a list and sorting them;
    reverse-traversal every time the tags are layered to ensure that all
    content is properly cataloged and the tags are appropriately nested;
    etc.

--- chin.py
+++ chin.py
@@ -36,13 +36,24 @@ def orderBss(x, y):
 	if y.getStart() != x.getStart():
 		return x.getStart() - y.getStart()
 	else:
-		return (x.getEnd() - x.getStart()) - (y.getEnd() - y.getStart())
+		return (y.getEnd() - y.getStart()) - (x.getEnd() - x.getStart())
+
+def openParallelByteSpans(index, i, n, reader, writer):
+	if i > len(bss):
+		return
+	if bss[n].getStart() != bss[i].getStart():
+		return
+	
+	tag = generateTagOpen(bss[i].getCorefId())
+	print tag
+
+	openParallelByteSpans(index, i, n+1, reader, writer)
 
 
 # Open file from argument
 raw = open(sys.argv[1], 'r')
 
-# Create a list of all lines from file
+# Create a list of all lines in the key
 lines = raw.readlines()
 
 # Create a list to hold the ByteSpan objects
@@ -52,51 +63,97 @@ bss = list()
 # 	Parse bytespan and corefid, put in object, place object in list
 for line in range(1, len(lines)):
 	words = lines[line].split()
-	byteRange = words[1].split(",")
-	id = filter(lambda x: x in '1234567890', words[5])
-	bs = ByteSpan(int(byteRange[0]), int(byteRange[1]), int(id))
-	bss.append(bs)
+	byteRange = words[1].split(",")	#parse bytespan
+	id = filter(lambda x: x in '1234567890', words[5])	#turn bytespan into numbers
+	bs = ByteSpan(int(byteRange[0]), int(byteRange[1]), int(id))	#store bytespan in object
+	bss.append(bs)	#add object to list
 
-# Sort the list
+# Sort the list. Begin with starting position of bytespan; in the
+# 	event of a tie, the largest bytespan comes first.
 bss = sorted(bss, cmp=orderBss)
 for i in bss:
 	i.printargs()
-sys.exit()
 
-# Create graph that tells how much the offset is at any given point
+# Create graph that tells how much the offset is at any given point in the file.
+# 	Adding the <span> tags to the HTML file changes how many bytes into the file
+# 	a word is; This graph is used to tell how many bytes the offset at any point
+# 	in the file is.
 graph = list()
 fileSize = os.path.getsize(sys.argv[2])
 for i in range(0, fileSize):
 	graph.append(0)
 
-# Create the html file
+# Open new html file, begin writing basic template data to it.
 overlay = open("corefoverlay.html", 'w')
 mirror = open(sys.argv[2], 'rb')
 start = "<html><head></head>\n<body>\n"
 overlay.write(start)
 
+# Update offset graph
 for n in range(len(graph)):
 	graph[n] += len(start)
 
+
 index = 0
-# Overlay HTML with divs
+for n in range(len(bss)):
+	# Write to overlay until beginning of next bytespan
+	while index < bss[n].getStart():
+		c = mirror.read(1)
+		print c
+		index+=1
+
+	print index, bss[n].getEnd(), bss[n+1].getStart()
+
+	tag = generateTagOpen(bss[n].getCorefId())
+	print tag
+
+	i = n+1
+
+	# Traverse list until we find a non-tied bytespan
+	while i < len(bss) and bss[n].getStart() == bss[i].getStart():
+		tag = generateTagOpen(bss[i].getCorefId())
+		print tag
+		i+=1
+
+	# Write the content until the end of that bytespan: then
+	tmp = i
+	while tmp > n:
+		while index <= bss[i].getEnd():
+			print mirror.read(1)
+			index+=1
+		tag = generateTagClose()
+		print tag
+		tmp-=1
+	
+	#sys.exit()
+
+sys.exit()
+
+
+index = 0
+# Begin overlay work on the HTML file.
 for item in bss:
-	# reset if bytespan is before current position
+	# reset if next bytespan is before current position
 	if item.getStart() < index:
-		mirror.seek(item.getStart())
-		overlay.seek(item.getStart())
-		index = item.getStart()
+		mirror.seek(item.getStart()+graph[item.getStart()])
+		overlay.seek(item.getStart()+graph[item.getStart()])
+		index = item.getStart()+graph[item.getStart()]
 
 	# while placeholder <= start of bytespan
 	while index < item.getStart():
-		overlay.write(mirror.read(1))
+		#overlay.write(mirror.read(1))
+		c = mirror.read(1)
+		print c
+		overlay.write(c)
 		index+=1
 
 	overlay.write(generateTagOpen(item.getCorefId()))
 	
 	# while placeholder in bytespan
 	while index <= item.getEnd():
-		overlay.write(mirror.read(1))
+		#overlay.write(mirror.read(1))
+		c = mirror.read(1)
+		overlay.write(c)
 		graph[index] += 15 + len(str(item.getCorefId()))
 		index+=1
 
@@ -108,4 +165,3 @@ for item in bss:
 
 
 overlay.write("\n</body>\n</html>")
-print graph
