5dd6352bda215bf53b0cdcc16b2cf1be376a987a
Alex Clemmer <clemmer.alexander@gmail.com>
2010-06-21 01:25:31 -0600

    Fixed the overlay buffer writer so that the tags match up
    Previously, the ends and beginnings were being chopped off when the
    buffer was loaded in and subtracted out; the reason for this was the
    use of "<" rather than "<=" in a while loop, which causes subsequent
    compensation problems elsewhere. This should be fixed now.

--- chin.py
+++ chin.py
@@ -3,13 +3,13 @@ import sys
 import os
 
 # Class that holds information for every bytespan.
-# 	A bytespan is typically a NP that is associated with a coref
-# 	chain. The actual bytespan of a Bytespan object refers to the
-# 	literal place in a document at which the NP resides: for example,
-# 	1,12 is a bytespan that begins at 1 and ends at 12. The bytespan
-# 	object also keeps track of the coref ID associated with the
-# 	bytespan, which essentially indicates to which coref chain the
-# 	NP belongs.
+#	 A bytespan is typically a NP that is associated with a coref
+#	 chain. The actual bytespan of a Bytespan object refers to the
+#	 literal place in a document at which the NP resides: for example,
+#	 1,12 is a bytespan that begins at 1 and ends at 12. The bytespan
+#	 object also keeps track of the coref ID associated with the
+#	 bytespan, which essentially indicates to which coref chain the
+#	 NP belongs.
 class ByteSpan:
 	def __init__(self, start, end, corefId):
 		self.start = start
@@ -30,16 +30,16 @@ class ByteSpan:
 		return self.corefId
 
 # Generates a span tag with an id specified in the formal params.
-# 	Each bytespan object has covers a span of bytes. For example, the
-# 	bytespan 0,12 covers the bytes from 0 - 12. Each of these objects
-# 	is associated with a coref chain, indicated by the coref ID. The
-# 	intended use of this function is to generate a span tag whose id
-# 	is the same as the bytespan object's coref ID.
+#	 Each bytespan object has covers a span of bytes. For example, the
+#	 bytespan 0,12 covers the bytes from 0 - 12. Each of these objects
+#	 is associated with a coref chain, indicated by the coref ID. The
+#	 intended use of this function is to generate a span tag whose id
+#	 is the same as the bytespan object's coref ID.
 def generateTagOpen(id):
 	return "<span class=\"" + str(id) + "\">"
 
 # Generates a closing for a span tag.
-# 	Intended to be close a tag that was openend with generateTagOpen()
+#	 Intended to be close a tag that was openend with generateTagOpen()
 def generateTagClose():
 	return "</span>"
 
@@ -47,10 +47,10 @@ def generateJs():
 	print "this is a stub for later"
 
 # The ordering function that helps sort() sort the bss objects. It first
-# 	looks at initial position, indicated by bss[anything].getStart(); if
-# 	one of them starts first, that one should come before the other in
-# 	the list. If they tie, the one that has the longest bytespan comes
-# 	first.
+#	 looks at initial position, indicated by bss[anything].getStart(); if
+#	 one of them starts first, that one should come before the other in
+#	 the list. If they tie, the one that has the longest bytespan comes
+#	 first.
 def orderBss(x, y):
 	if y.getStart() != x.getStart():
 		return x.getStart() - y.getStart()
@@ -58,30 +58,36 @@ def orderBss(x, y):
 		return (y.getEnd() - y.getStart()) - (x.getEnd() - x.getStart())
 
 # Updates the offset graph given a position (pos) and an amount (amt).
-# 	When writing the NP/coref chain overlay, we add span tags to the html,
-# 	which allows us to identify NPs as in one coref chain or another. The
-# 	graph tells us how many characters have been added to the document at
-# 	any given position. For example, if graph[x] holds the integer 43, we
-# 	know that there are 43 characters added before that position. Thus,
-# 	when we add to the overlay document, we know that we should seek to
-# 	the position x + 43, or whatever position we wanted, plus the 43 chars
-# 	we've added to the document.
+#	 When writing the NP/coref chain overlay, we add span tags to the html,
+#	 which allows us to identify NPs as in one coref chain or another. The
+#	 graph tells us how many characters have been added to the document at
+#	 any given position. For example, if graph[x] holds the integer 43, we
+#	 know that there are 43 characters added before that position. Thus,
+#	 when we add to the overlay document, we know that we should seek to
+#	 the position x + 43, or whatever position we wanted, plus the 43 chars
+#	 we've added to the document.
 def updateGraph(pos, updateAmt):
 	for placeholder in range(pos, len(graph)):
 		graph[placeholder] += updateAmt
 
 # Copies all characters in the overlay file from the point pos to EOF.
-# 	We use this to insert tags into the document; we don't want to
-# 	overwrite everything, but instead, to add some text, and preserve
-# 	whatever comes after this. The way to do this is to copy everything
-# 	after into a buffer, and then write it back afterwards. Because we
-# 	sort the bytespan objects in bss, this tends to be no more than a
-# 	few characters.
+#	 We use this to insert tags into the document; we don't want to
+#	 overwrite everything, but instead, to add some text, and preserve
+#	 whatever comes after this. The way to do this is to copy everything
+#	 after into a buffer, and then write it back afterwards. Because we
+#	 sort the bytespan objects in bss, this tends to be no more than a
+#	 few characters.
 def createBuffer(endPosOfBytespan):
 	readOverlay = open("corefoverlay.html", 'r')
 	readOverlay.seek(endPosOfBytespan, 0)
 	buffer = readOverlay.read()
 	return buffer
+
+def readBytes(start, stop):
+	readOverlay = open("corefoverlay.html", 'r')
+	readOverlay.seek(start, 0)
+	sentence = readOverlay.read(stop-start)
+	return sentence
 
 
 
@@ -95,7 +101,7 @@ lines = raw.readlines()
 bss = list()
 
 # Process each line in succession
-# 	Parse bytespan and corefid, put in object, place object in list
+#	 Parse bytespan and corefid, put in object, place object in list
 for line in range(1, len(lines)):
 	words = lines[line].split()
 	byteRange = words[1].split(",")	#parse bytespan
@@ -104,15 +110,15 @@ for line in range(1, len(lines)):
 	bss.append(bs)	#add object to list
 
 # Sort the list. Begin with starting position of bytespan; in the
-# 	event of a tie, the largest bytespan comes first.
+#	 event of a tie, the largest bytespan comes first.
 bss = sorted(bss, cmp=orderBss)
 #for i in bss:
 	#i.printargs()
 
 # Create graph that tells how much the offset is at any given point in the file.
-# 	Adding the <span> tags to the HTML file changes how many bytes into the file
-# 	a word is; This graph is used to tell how many bytes the offset at any point
-# 	in the file is.
+#	 Adding the <span> tags to the HTML file changes how many bytes into the file
+#	 a word is; This graph is used to tell how many bytes the offset at any point
+#	 in the file is.
 graph = list()
 fileSize = os.path.getsize(sys.argv[2])
 for i in range(0, fileSize):
@@ -122,7 +128,7 @@ for i in range(0, fileSize):
 write = open("corefoverlay.html", 'w')
 read = open(sys.argv[2], 'rb')
 start = "<html><head></head>\n<body>\n"
-print start
+#print start
 write.write(start)
 
 # Update offset graph
@@ -134,57 +140,62 @@ index = 0
 # Handle everything that happens before the first bytespan
 while index < bss[0].getStart():
 	c = read.read(1)
-	print c
+	#print c
 	write.write(c)
 	index+=1
 
 # Process 1 bytespan object:
-# 	1. Reset the var index, which we use to keep track of where our "cursor" is in the
-# 		original (raw) text file. Seek read's cursor to this point, as we want to start
-# 		reading from here. If you don't seek, then you end up continuing reading from the
-# 		last point you were at.
-# 	2. Seek to the byte in the overlay file that's delineated by [item].getStart()
-# 		Because we are adding tags, the location of the byte in the original file will
-# 		not correspond directly to the byte in the overlay file; to get the corresponding
-# 		byte, we must include the "offset" generated by the tags, which is stored in
-# 		the list called "graph". So the location of any byte in the overlay file can
-# 		be expressed as overlayPos = originalPos + graph[x], where the integer at graph[x]
-# 		contains the offset at the point x.
-# 	3. Copy everything from the position we will be inserting to until the end of the
-# 		overlay file into a buffer. This may sound expensive, but remember that the
-# 		bytespan objects are sorted, so the cost of this is actually usually only a few
-# 		characters.
-# 	4. Insert opening tag that will hold the NP in the given bytespan
-# 	5. Update the graph that keeps track of offset
-# 	6. Write content of tag
-# 	7. Insert closing tag
-# 	8. Update graph that keeps track of offset
-# 	9. Copy back the buffer
+#	 1. Reset the var index, which we use to keep track of where our "cursor" is in the
+#		 original (raw) text file. Seek read's cursor to this point, as we want to start
+#		 reading from here. If you don't seek, then you end up continuing reading from the
+#		 last point you were at.
+#	 2. Seek to the byte in the overlay file that's delineated by [item].getStart()
+#		 Because we are adding tags, the location of the byte in the original file will
+#		 not correspond directly to the byte in the overlay file; to get the corresponding
+#		 byte, we must include the "offset" generated by the tags, which is stored in
+#		 the list called "graph". So the location of any byte in the overlay file can
+#		 be expressed as overlayPos = originalPos + graph[x], where the integer at graph[x]
+#		 contains the offset at the point x.
+#	 3. Copy everything from the position we will be inserting to until the end of the
+#		 overlay file into a buffer. This may sound expensive, but remember that the
+#		 bytespan objects are sorted, so the cost of this is actually usually only a few
+#		 characters.
+#	 4. Insert opening tag that will hold the NP in the given bytespan
+#	 5. Update the graph that keeps track of offset
+#	 6. Write content of tag
+#	 7. Insert closing tag
+#	 8. Update graph that keeps track of offset
+#	 9. Copy back the buffer
+readBuffer = open("corefoverlay.html", 'r')
 bssItem = 0
-while bssItem < 3:
-	index = bss[bssItem].getStart() # 1
+while bssItem < 4:
+	# TODO: we should write all the characters up to the next tag opening if they are not included in a bytespan at all. We might check this by looking at previous bytespans, or the offset graph.
+	index = bss[bssItem].getStart()
 	read.seek(index)
-	write.seek(bss[bssItem].getStart() + graph[bss[bssItem].getStart()]) # 2
-	buffer = createBuffer(bss[bssItem].getEnd() + graph[bss[bssItem].getEnd()-1]) # 3
-	print "buffer: " + str(bss[bssItem].getEnd()) + " " + str(graph[bss[bssItem].getEnd()]) + buffer
-	if bssItem == 3:
-		print str(bss[bssItem].getEnd() + graph[bss[bssItem].getEnd()])
-		print graph
-		sys.exit()
+	write.seek(index + graph[index])
+	
+	readBuffer.seek(bss[bssItem].getEnd()+1 + graph[bss[bssItem].getEnd()])
+	buffer = readBuffer.read()
+	
 	tag = generateTagOpen(bss[bssItem].getCorefId())
+	updateGraph(index, len(tag))
+	write.write(tag)
 	print tag
-	write.write(tag) # 4
-	updateGraph(bss[bssItem].getStart(), len(generateTagOpen(bss[bssItem].getCorefId()))) # 5
-	while index < bss[bssItem].getEnd(): # 6
+	
+	holder = ""
+	while index <= bss[bssItem].getEnd():
 		c = read.read(1)
-		print c
 		write.write(c)
-		index+=1
+		holder += c
+		index += 1
+	print holder
+	
 	tag = generateTagClose()
+	updateGraph(index, len(tag))
+	write.write(tag)
 	print tag
-	write.write(tag) # 7
-	updateGraph(bss[bssItem].getEnd(), len(generateTagClose())) # 8
-	write.seek(bss[bssItem].getEnd() + graph[bss[bssItem].getEnd()]) # 9
+	
 	write.write(buffer)
-
+	print buffer
+	
 	bssItem+=1
