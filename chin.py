a26099cf89bdce93076618c3dd03c467fcccf8d7
Alex Clemmer <clemmer.alexander@gmail.com>
2010-06-24 00:58:22 -0600

    Span tags should now close properly
    Previously we would generate opening tags, but never closing tags. This
    problem appears to be corrected now, with properly terminated span tags
    that denote coref chains.

--- chin.py
+++ chin.py
@@ -54,19 +54,6 @@ def orderBss(x, y):
 	else:
 		return (y.getEnd() - y.getStart()) - (x.getEnd() - x.getStart())
 
-# Updates the offset graph given a position (pos) and an amount (amt).
-#	 When writing the NP/coref chain overlay, we add span tags to the html,
-#	 which allows us to identify NPs as in one coref chain or another. The
-#	 graph tells us how many characters have been added to the document at
-#	 any given position. For example, if graph[x] holds the integer 43, we
-#	 know that there are 43 characters added before that position. Thus,
-#	 when we add to the overlay document, we know that we should seek to
-#	 the position x + 43, or whatever position we wanted, plus the 43 chars
-#	 we've added to the document.
-def updateGraph(pos, updateAmt):
-	for placeholder in range(pos, len(graph)):
-		graph[placeholder] += updateAmt
-
 # Copies all characters in the overlay file from the point pos to EOF.
 #	 We use this to insert tags into the document; we don't want to
 #	 overwrite everything, but instead, to add some text, and preserve
@@ -116,27 +103,13 @@ for line in range(1, len(lines)):
 # Sort the list. Begin with starting position of bytespan; in the
 #	 event of a tie, the largest bytespan comes first.
 bss = sorted(bss, cmp=orderBss)
-#for i in bss:
-	#i.printargs()
-
-# Create graph that tells how much the offset is at any given point in the file.
-#	 Adding the <span> tags to the HTML file changes how many bytes into the file
-#	 a word is; This graph is used to tell how many bytes the offset at any point
-#	 in the file is.
-graph = list()
-fileSize = os.path.getsize(sys.argv[2])
-for i in range(0, fileSize):
-	graph.append(0)
 
 # Open new html file, begin writing basic template data to it.
 write = open("corefoverlay.html", 'w')
 read = open(sys.argv[2], 'rb')
 start = "<html><head></head>\n<body>\n"
-#print start
 write.write(start)
 
-# Update offset graph
-updateGraph(0, len(start))
 
 
 
@@ -148,14 +121,25 @@ updateGraph(0, len(start))
 
 char = True
 charIndex = 0
-bytespanIndex = 0
+bytespanOpen = 0
+bytespanClose = 0
+bytespanStack = list()
+
 while char:
 	char = read.read(1)
-	while bytespanIndex < len(bss) and bss[bytespanIndex].getStart() == charIndex:
-		write.write(generateTagOpen(bss[bytespanIndex].getCorefId()))
-		bytespanIndex+=1
+	while bytespanOpen < len(bss) and bss[bytespanOpen].getStart() == charIndex:
+		write.write(generateTagOpen(bss[bytespanOpen].getCorefId()))
+		bytespanStack.insert(0, bss[bytespanOpen])
+		bytespanOpen+=1
 
-	write.write(char)
+	while len(bytespanStack) != 0 and bytespanStack[0].getEnd() == charIndex:
+		write.write(generateTagClose())
+		bytespanStack.pop(0)
+
+	if char == "\n":
+		write.write("<p>\n")
+	else:
+		write.write(char)
 	charIndex+=1
 
 sys.exit()
