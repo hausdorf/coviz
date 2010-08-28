4b451a8f8a974728d2c7fa961c63fa9d042b9c13
Alex Clemmer <clemmer.alexander@gmail.com>
2010-08-28 01:26:29 -0600

    Fix syntax error in method call

--- chin.py
+++ chin.py
@@ -245,7 +245,7 @@ write.write("<div id=\"tracking\">")
 while char:
 	char = read.read(1) #1
 	while bytespanIndex < len(bss) and bss[bytespanIndex].getStart() == charIndex: #2
-		write.write(generateTagOpen(bss[bytespanIndex].getCorefId(), bss[bytespanIndex].getLevelsNested)) #2a
+		write.write(generateTagOpen(bss[bytespanIndex].getCorefId(), bss[bytespanIndex].getLevelsNested())) #2a
 		bytespanStack.insert(0, bss[bytespanIndex]) #2b
 		bytespanIndex+=1
 
