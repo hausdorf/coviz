248c955e95a7483157551a00e2868dfa22974d2c
Alex Clemmer <clemmer.alexander@gmail.com>
2010-09-07 22:36:54 -0600

    Change the current color every time there we click an NP

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ var allHTMLTags = new Array();
 var recentlyModified = false;
 var lastClass = "";
 var toMarkup = new Array();
+var currColor = "333333"
 
 function peek(theClass) {
 	if(!recentlyModified) {
@@ -26,17 +27,18 @@ function peek(theClass) {
 }
 
 function cycle() {
-	var tmpClass = toMarkup.pop();
 	for(var i=0;i<toMarkup.length;i++) {
+		var tmpClass = toMarkup.pop();
 		var allHTMLTags = document.getElementsByTagName("*");
 		for(i=0; i<allHTMLTags.length;i++) {
 		  if(allHTMLTags[i].className==(tmpClass+"-tracking")) {
-		    allHTMLTags[i].style.color="red";
+		    allHTMLTags[i].style.color=currColor;
 		    allHTMLTags[i].style.fontWeight="bold";
 		    allHTMLTags[i].style.textDecoration="underline";
 		  }
 		}
 	}
+	currColor = (parseInt(currColor, 16) + 30000).toString(16);
 }
 
 function printAttributes(id, start, end) {
